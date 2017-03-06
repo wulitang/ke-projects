c37f85fa53538181d8b1b950a50b7dddf581a684
liyuan <1009102873@qq.com>
2017-03-06 17:19:28 +0800

    首页修改

--- museum/marry/js/index.js
+++ museum/marry/js/index.js
@@ -1,3 +1,26 @@
 Zepto(function($){
-    
+    $(".index-menu").click(function () {
+        $(".index-menu-list").toggleClass("show");
+        $(".index-min").toggleClass("show-left")
+    });
+    $(".index-menu-list li").click(function(){
+        $(this).addClass("active").siblings("li").removeClass("active");
+        $(".index-menu-list").toggleClass("show");
+        $(".index-min").toggleClass("show-left")
+    });
+    $(".add").click(function () {
+        var i = $(this).parent().find("span").html()||0;
+        i++;
+        $(this).siblings().show();
+        $(this).parent("div").find("span").html(i);
+    });
+    $(".reduce").click(function () {
+        var pic =$(this).parent().find("span").html();
+        pic--;
+        $(this).parent().find("span").html(pic);
+        if(pic<1){
+            $(this).parent("div").find("span").hide();
+            $(this).hide();
+        }
+    });
 });
