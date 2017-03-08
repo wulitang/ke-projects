f24e8965186bc0d5bbd158e5e1e6e1e5ecad77a1
hepeng-ice <287993389@qq.com>
2017-03-08 14:07:25 +0800

    详情页修改

--- museum/marry/js/details.js
+++ museum/marry/js/details.js
@@ -1,13 +1,19 @@
 //详情页js
 Zepto(function($){
     //购物数量加减
-    var num=$(".mui_number span").text();
-    $(".add").click(function(){
-        $(".mui_number span").text(++num);
+    $(".add").click(function () {
+        var i = $(this).parent().find("span").html()||0;
+        i++;
+        $(this).siblings().css("display","inline-block");
+        $(this).parent("div").find("span").html(i);
     });
-    $(".reduce").click(function(){
-        if(num>1){
-            $(".mui_number span").text(--num);
+    $(".reduce").click(function () {
+        var pic =$(this).parent().find("span").html();
+        pic--;
+        $(this).parent().find("span").html(pic);
+        if(pic<1){
+            $(this).parent("div").find("span").hide();
+            $(this).hide();
         }
     });
 });
